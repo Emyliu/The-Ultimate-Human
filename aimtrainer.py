1b335d8db3638c5b5fabb2113cb8be2296475a26
Emyliu <32317817+Emyliu@users.noreply.github.com>
2020-06-19 10:56:23 -0400

    Re-use the blue line to find the pixel position of the target rather than taking more pictures. Speed is greatly improved, ~235ms per tgt.

--- aimtrainer.py
+++ aimtrainer.py
@@ -4,23 +4,22 @@ import cv2
 from PIL import ImageGrab
 
 blueLine = ImageGrab.grab(bbox=(1,214,939,215))
+
 ct = 0
 
 while True:
     for y in range(214,703,75):
             img = ImageGrab.grab(bbox=(1,y,939,y + 1))
             if img != blueLine:
-                for x in range(40, 940, 80):
-                    small = ImageGrab.grab(bbox=(x,y,x + 1, y + 1))
-                    smallNp = np.array(small)
-                    clickTest = smallNp == np.array([43, 135, 209])
-                    if not clickTest.all():
+                line = np.array(img)
+                bluePixel = np.array([43, 135, 209])
+                for x in range(40, 940, 75):
+                    small = line[0][x]
+                    bluePixelTester = small == bluePixel
+                    if not bluePixelTester.all():
                         pyautogui.click(x, y)
                         ct += 1
-                        if ct >= 30:
+                        if ct >= 50:
                             print("leave")
                             exit()
 
-
-
-print("Done")
