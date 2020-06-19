6a985f811039fd361b568f77585fb3d85383a065
Emyliu <32317817+Emyliu@users.noreply.github.com>
2020-06-19 11:25:48 -0400

    Don't really need to compare with a blue pixel to tell that we're not blue. Just look at the first R,G,B value.

--- aimtrainer.py
+++ aimtrainer.py
@@ -3,20 +3,18 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
-blueLine = ImageGrab.grab(bbox=(1,214,939,215))
+blueLine = ImageGrab.grab(bbox=(1,217,939,218))
 
 ct = 0
 
 while True:
-    for y in range(214,703,75):
+    for y in range(217,703,75):
             img = ImageGrab.grab(bbox=(1,y,939,y + 1))
             if img != blueLine:
                 line = np.array(img)
-                bluePixel = np.array([43, 135, 209])
                 for x in range(40, 940, 75):
-                    small = line[0][x]
-                    bluePixelTester = small == bluePixel
-                    if not bluePixelTester.all():
+                    notBlue = line[0][x][0] > 60
+                    if notBlue:
                         pyautogui.click(x, y)
                         ct += 1
                         if ct >= 50:
